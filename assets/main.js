2a4cbe086d878665bbe1d0f5ccc3e2a567fe868f
mohamedelsayed31 <me6877818@gmail.com>
2025-09-14 13:03:45 +0300

    Add files via upload

---
+++ assets/main.js
@@ -0,0 +1,14 @@
+// Scroll Reveal
+const sr = ScrollReveal ({
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    reset: true
+    // reset: false
+})
+
+// sr.reveal('.section-header', {delay: 400, origin: 'top'})
+sr.reveal('.about-content', {delay: 50, origin: 'top'})
+// sr.reveal('.portfolio-content', {delay: 50, origin: 'top'})
+sr.reveal('.offer-content', {delay: 50, origin: 'top'})
+sr.reveal('.service-item-img', {delay: 50, origin: 'right'})
